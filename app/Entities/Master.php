30b9c4a297e80cdd8e3e9793672b35b6229c8ab3
rama2z <ramkumar@a2z.health>
2021-01-28 15:55:16 +0530

    Diet Master added

--- app/Entities/Master.php
+++ app/Entities/Master.php
@@ -25,41 +25,7 @@ class Master extends BaseModel
         
     ];
 
-    /**
-     * The attributes excluded from the model's JSON form.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        
-    ];
-
-    /**
-     * The attributes that should be updated on patch method.
-     *
-     * @var array
-    */
-    protected $partialFillable = [
-        
-    ];
-
-    /**
-     * The attributes that should be mutated to dates.
-     *
-     * @var array
-    */
-    protected $dates = [
-        
-    ];
-
-    /**
-     * The event map for the model.
-     *
-     * @var array
-    */
-    protected $dispatchesEvents = [
-        
-    ];
+    public $timestamps = false;
 
     public function applyFilters($model, $isPluck)
     {
