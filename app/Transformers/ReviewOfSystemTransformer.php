7beaad285ca922e7c36d40f5a303b2fdd8fea55b
rama2z <ramkumar@a2z.health>
2021-02-04 18:13:39 +0530

    ROS worflow

---
+++ app/Transformers/ReviewOfSystemTransformer.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Transformers;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ReviewOfSystemTransformer extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+    */
+
+    public function toArray($request): array
+    {
+        return [
+            'id'            =>  $this->id,
+            'patient_id'    =>  $this->patient_id,
+            'consult_id'    =>  $this->consult_id,
+            'slug'          =>  $this->slug,
+            'status'        =>  $this->status,
+            'values'        =>  $this->values
+        ];
+    }
+}
