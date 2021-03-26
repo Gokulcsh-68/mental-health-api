19a6a4f4ca823ba1cb2493879be1c1b82f56a0bf
Philip Martin <philipkarthick@gmail.com>
2021-03-26 17:14:36 +0530

    Email header logo updated

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,7 +1,7 @@
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
-<img src="https://school.a2zhealth.in/staff/assets/media/logos/logo-dark.svg" class="logo" alt="{{ config('app.name') }}">
+<img src="https://a2zschoolfolio.s3.ap-south-1.amazonaws.com/a2z-health-school-logo.png" class="logo" alt="{{ config('app.name') }}">
 </a>
 </td>
 </tr>
