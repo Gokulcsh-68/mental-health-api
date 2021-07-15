224b3111267098cb370a9f48ad9927e1ffe60914
vijaya2z <61727460+vijaya2z@users.noreply.github.com>
2021-07-15 17:23:26 +0530

    Update Camera.php

--- app/Entities/Camera.php
+++ app/Entities/Camera.php
@@ -91,4 +91,20 @@ class Camera extends BaseModel
 
         return null;
     }
+
+
+    public function applyFilters($model, $isPluck)
+    {
+        $model = parent::applyFilters($model, $isPluck);
+        $request = app('request');
+    
+
+        if($request->user()->role->code == 'hospital'){
+
+            $model->where('hospital_id', $request->get('hospital_id'));
+        }
+
+        return $model;
+    }
+
 }
