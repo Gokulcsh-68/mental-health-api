795e3cd948ced8787a4b5202710292446b1d808c
vijaya2z <61727460+vijaya2z@users.noreply.github.com>
2021-12-28 13:52:38 +0530

    Create UpdateFormsroleSeeder.php

---
+++ database/seeders/UpdateFormsroleSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use DB;
+use Illuminate\Database\Seeder;
+
+class UpdateFormsroleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+       
+
+        $slug_data = ['healthy-heart','psychiatric-exam-symptoms','psychiatric-exam-anger','psychiatric-exam-anxiety','psychiatric-exam-depression','psychiatric-exam-mania','stroke-scale-symptoms-signs','stroke-scale-nih','stroke-scale-tpa-contraindications','stroke-scale-ischemic'];
+        $roles = ["admin", "provider", "hospitalgroup", "hospital", "folio"];
+
+        DB::table('forms')->WhereIN('slug', $slug_data)->update(['role_code'=>$roles]);
+
+    }
+}
