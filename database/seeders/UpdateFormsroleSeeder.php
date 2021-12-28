9d9945a4abf24aebf7fd2f24195f6017f0c879f8
vijaya2z <61727460+vijaya2z@users.noreply.github.com>
2021-12-28 13:56:46 +0530

    Update UpdateFormsroleSeeder.php

--- database/seeders/UpdateFormsroleSeeder.php
+++ database/seeders/UpdateFormsroleSeeder.php
@@ -16,7 +16,7 @@ class UpdateFormsroleSeeder extends Seeder
     {
        
 
-        $slug_data = ['healthy-heart','psychiatric-exam-symptoms','psychiatric-exam-anger','psychiatric-exam-anxiety','psychiatric-exam-depression','psychiatric-exam-mania','stroke-scale-symptoms-signs','stroke-scale-nih','stroke-scale-tpa-contraindications','stroke-scale-ischemic'];
+        $slug_data = ['healthy-heart','psychiatric-exam-symptoms','psychiatric-exam-anger','psychiatric-exam-anxiety','psychiatric-exam-depression','psychiatric-exam-mania','stroke-scale-symptoms-signs','stroke-scale-nih','stroke-scale-tpa-contraindications','stroke-scale-ischemic', 'covid_self_assessment'];
         $roles = ["admin", "provider", "hospitalgroup", "hospital", "folio"];
 
         DB::table('forms')->WhereIN('slug', $slug_data)->update(['role_code'=>$roles]);
