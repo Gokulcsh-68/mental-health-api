96cde10dbfe8d08ecf65e6d9bf0584aac2f4aacf
vijaya2z <avijaydeva@cureselecthealth.com>
2023-11-04 01:13:19 +0530

    Update UpdateVitalsSeeder.php

--- database/seeders/UpdateVitalsSeeder.php
+++ database/seeders/UpdateVitalsSeeder.php
@@ -24,6 +24,7 @@ class UpdateVitalsSeeder extends Seeder
             [ 'master_type_slug' => 'vitals', 'name' => 'Keytone', 'slug' => 'keytone'],
             [ 'master_type_slug' => 'vitals', 'name' => 'Uric Acid', 'slug' => 'uric_acid'],
             [ 'master_type_slug' => 'vitals', 'name' => 'Spirometer', 'slug' => 'spirometer'],
+            [ 'master_type_slug' => 'health', 'name' => 'Dental', 'slug' => 'dental'],
         ];
         
         DB::table('masters')->insertOrIgnore($master_types);
