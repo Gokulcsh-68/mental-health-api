46c62ab04b156713c53676f9a2f6c19daf37124c
rkcsh <rk@cureselecthealthcare.com>
2025-01-11 15:32:06 +0530

    adding

--- app/Entities/PatientHealth.php
+++ app/Entities/PatientHealth.php
@@ -279,9 +279,10 @@ class PatientHealth extends BaseModel
 
             // Prescription
             if($request->get('slug') == 'prescription'){
-                $model->whereRaw("`values`->'$[*].medicine_name' LIKE ?", ['%' . $request->get('searchkey'). '%'])
-                    ->orWhereRaw("`values`->'$[*].medicine_name' LIKE ?", ['%' . $request->get('searchkey'). '%']);
-
+                $model->whereRaw(
+                    "JSON_EXTRACT(`values`, '$[*].medicine_name') LIKE ?",
+                    ['%' . $request->get('searchkey') . '%']
+                );
             }
         }
 
