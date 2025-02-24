e714982237069c0326c9a2d32e2500c207056761
rkcsh <rk@cureselecthealthcare.com>
2025-02-24 13:00:22 +0530

    Adding hospital missing seeder

--- database/seeders/SpecialityTableSeeder.php
+++ database/seeders/SpecialityTableSeeder.php
@@ -47,8 +47,31 @@ class SpecialityTableSeeder extends Seeder
             ['master_type_slug' => 'speciality', 'slug' => 'otorhinolaryngology','name' => 'Otorhinolaryngology', 'is_active' => 1],
             ['master_type_slug' => 'speciality', 'slug' => 'andrology','name' => 'Andrology', 'is_active' => 1],
             ['master_type_slug' => 'speciality', 'slug' => 'rheumatology','name' => 'Rheumatology', 'is_active' => 1],
-    	];
+            
+            ['master_type_slug' => 'speciality', 'slug' => 'infectious-disease', 'name' => 'Infectious Disease', 'is_active' => 1],
+            ['master_type_slug' => 'speciality', 'slug' => 'nuclear-medicine', 'name' => 'Nuclear Medicine', 'is_active' => 1],
+            ['master_type_slug' => 'speciality', 'slug' => 'sports-medicine', 'name' => 'Sports Medicine', 'is_active' => 1],
+            ['master_type_slug' => 'speciality', 'slug' => 'plastic-surgery', 'name' => 'Plastic Surgery', 'is_active' => 1],
+            ['master_type_slug' => 'speciality', 'slug' => 'vascular-surgery', 'name' => 'Vascular Surgery', 'is_active' => 1],
+            ['master_type_slug' => 'speciality', 'slug' => 'thoracic-surgery', 'name' => 'Thoracic Surgery', 'is_active' => 1],
+            ['master_type_slug' => 'speciality', 'slug' => 'colorectal-surgery', 'name' => 'Colorectal Surgery', 'is_active' => 1],
+            ['master_type_slug' => 'speciality', 'slug' => 'critical-care-medicine', 'name' => 'Critical Care Medicine', 'is_active' => 1],
+            ['master_type_slug' => 'speciality', 'slug' => 'medical-genetics', 'name' => 'Medical Genetics', 'is_active' => 1],
+            ['master_type_slug' => 'speciality', 'slug' => 'pain-medicine', 'name' => 'Pain Medicine', 'is_active' => 1],
+            ['master_type_slug' => 'speciality', 'slug' => 'internal-medicine', 'name' => 'Internal Medicine', 'is_active' => 1],
+            ['master_type_slug' => 'speciality', 'slug' => 'diabetology', 'name' => 'Diabetology', 'is_active' => 1],
+            ['master_type_slug' => 'speciality', 'slug' => 'internal-medicine-diabetology', 'name' => 'Internal Medicine and Diabetes', 'is_active' => 1],
+            ['master_type_slug' => 'speciality', 'slug' => 'orthopedics', 'name' => 'Orthopedics', 'is_active' => 1],
+            ['master_type_slug' => 'speciality', 'slug' => 'clinical-pharmacology', 'name' => 'Clinical Pharmacology', 'is_active' => 1],
+            ['master_type_slug' => 'speciality', 'slug' => 'occupational-medicine', 'name' => 'Occupational Medicine', 'is_active' => 1],
+            ['master_type_slug' => 'speciality', 'slug' => 'preventive-medicine', 'name' => 'Preventive Medicine', 'is_active' => 1],
+            ['master_type_slug' => 'speciality', 'slug' => 'public-health', 'name' => 'Public Health', 'is_active' => 1],
+            ['master_type_slug' => 'speciality', 'slug' => 'sleep-medicine', 'name' => 'Sleep Medicine', 'is_active' => 1],
+            ['master_type_slug' => 'speciality', 'slug' => 'transplant-surgery', 'name' => 'Transplant Surgery', 'is_active' => 1],
+            ['master_type_slug' => 'speciality', 'slug' => 'oral-and-maxillofacial-surgery', 'name' => 'Oral and Maxillofacial Surgery', 'is_active' => 1],
+        ];
+        
 
-        DB::table('masters')->insert($speciality);
+        DB::table('masters')->insertOrIgnore($speciality);
     }
 }
